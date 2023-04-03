2e11601eb2eef168114ddc5a7639cf5c1f55e037
Murilo Alves Zapiello <54750759805@info.local>
2023-04-03 17:22:28 -0300

    aula dia 03/04

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿//crie um menu de opções e peça para o usuário escolher uma das opções
+//a depender da opção, exiba uma mensagem se gostaria de acrescentar gelo
+//se sim, exiba uma mensagem que a bebida em questão será com adicional de gelo
+//caso contrário, a bebida é sem gelo adicional
+
+
+Console.BackgroundColor = ConsoleColor.DarkBlue;
+Console.WriteLine(@$"
+-------------------------------------
+|      ~~Cardápio de bebidas~~      | 
+|                                   |
+|    sucos: 1)laranja               |
+|           2)uva                   |
+|           3)manga                 |
+|           4)caja                  |
+|           5)caju                  |
+|                                   |
+|  Refrigerantes: 6) coca-cola      |
+|                 7) Pepsi          |
+|                 8) Guarana        |
+|                 9) Fanta uva      |
+|                 10) Fanta laranja |
+-------------------------------------    
+");
+Console.ResetColor();
+
+Console.WriteLine($"Digite o número da bebida desejada: ");
+int numBebida = int.Parse(Console.ReadLine());
+
+string nomeBebida = ".........."; 
+
+switch (nomeBebida)
+{
+    case 1:
+        Console.WriteLine($"Suco de laranja");
+        
+        break;
+    default:
+        break;
+}
+
+
+
+
+
