f371b5bfea1feb6a14aa0c97ec3141faa06f86a8
Murilo Alves Zapiello <54750759805@info.local>
2023-04-04 14:36:01 -0300

    termino da atividade

--- Program.cs
+++ Program.cs
@@ -27,17 +27,83 @@ Console.ResetColor();
 Console.WriteLine($"Digite o número da bebida desejada: ");
 int numBebida = int.Parse(Console.ReadLine());
 
-string nomeBebida = ".........."; 
+int nomeBebida = 0;
 
-switch (nomeBebida)
+switch (numBebida)
 {
     case 1:
         Console.WriteLine($"Suco de laranja");
-        
+
         break;
+
+    case 2:
+        Console.WriteLine($"Suco de uva");
+
+        break;
+
+    case 3:
+        Console.WriteLine($"Suco de manga");
+
+        break;
+
+
+    case 4:
+        Console.WriteLine($"Suco de caja");
+
+        break;
+
+    case 5:
+        Console.WriteLine($"Suco de caju");
+
+        break;
+    case 6:
+        Console.WriteLine($"coca-cola");
+
+        break;
+    case 7:
+        Console.WriteLine($"pepsi");
+
+        break;
+    case 8:
+        Console.WriteLine($"guaraná");
+
+        break;
+    case 9:
+        Console.WriteLine($"fanta uva");
+
+        break;
+    case 10:
+        Console.WriteLine($"fanta laranja");
+
+        break;
+
     default:
+        Console.WriteLine($"O número digitado não corresponde a nenhuma opção de bebida.");
+
         break;
 }
+
+Console.WriteLine($"Deseja por gelo por mais 50 centavos? digite ~~s~~ para sim e ~~n~~ para não");
+string simounao = Console.ReadLine();
+
+switch (simounao)
+{
+    case "s":
+        Console.WriteLine($"Ok será R$2,50 tenha um bom dia");
+
+        break;
+
+    case "n":
+        Console.WriteLine($"Ok R$2 será tenha um bom dia");
+
+        break;
+    default:
+        Console.WriteLine($"Eu fui bem claro utilize ~~s~~ para sim e ~~n~~ para não... repita o processo!");
+        
+        break;
+}
+
+
 
 
 
